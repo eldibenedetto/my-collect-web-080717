a057d9b41d077b1f02ff5c26e4e219f67d7d05e5
Eugene DiBenedetto <eldibenedetto@gmail.com>
2017-07-27 16:19:26 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+    i =0
+    a = []
+    while i < array.length
+      a << yield(array[i])
+      i += 1
+    end
+   a
+end
 
-
+#array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+#my_collect(array) do |name|
+#  puts name
+#  name.split(" ").first
+#end
